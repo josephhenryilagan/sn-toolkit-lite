1ea246883e7e0e4060495e551b575d61303fceba
Joseph Henry Ilagan <josephhenryilagan@gmail.com>
2026-04-03 01:21:11 +0800

    feature: added anti-cache

--- bookmark.js
+++ bookmark.js
@@ -1,6 +1,7 @@
 javascript: (function () {
     const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/josephhenryilagan/servicenow-toolkit-lite@main/ServiceNow%20Toolkit.js';
+    const antiCache = '?time=' + new Date().getTime();
+    script.src = 'https://cdn.jsdelivr.net/gh/josephhenryilagan/servicenow-toolkit-lite@main/ServiceNow%20Toolkit.js' + antiCache;
     script.type = 'text/javascript';
     document.head.appendChild(script);
 })();
