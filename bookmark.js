1c3153c5e6da78670854b027f0f69f9c30b38dd0
Joseph Henry Ilagan <josephhenryilagan@gmail.com>
2026-05-01 02:41:59 +0800

    feature: removed anti-cache

--- bookmark.js
+++ bookmark.js
@@ -1,7 +1,6 @@
 javascript: (function () {
     const script = document.createElement('script');
-    const antiCache = '?time=' + new Date().getTime();
-    script.src = 'https://cdn.jsdelivr.net/gh/josephhenryilagan/sn-toolkit-lite@main/sn-toolkit-lite.js' + antiCache;
+    script.src = 'https://cdn.jsdelivr.net/gh/josephhenryilagan/sn-toolkit-lite@main/sn-toolkit-lite.js';
     script.type = 'text/javascript';
     document.head.appendChild(script);
 })();
